852c675d89fcb576c112f208052b7ad5a52831da
Getsgoor <club-zitouni@hotmail.fr>
2025-10-20 16:00:44 +0200

    Fix changing the type

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -5,11 +5,11 @@
  */
 int main(void)
 {
-	printf("Size of a char: %zu byte(s), sizeof(char)");
-	printf("Size of a int: %zu byte(s), sizeof(int)");
-	printf("Size of a long int: %zu byte(s), sizeof(long int)");
-	printf("Size of a long long int: %zu byte(s), sizeof(long long int)");
-	printf("Size of a float: %zu byte(s), sizeof(float)");
+	printf("Size of a char: %d byte(s), sizeof(char)");
+	printf("Size of a int: %d byte(s), sizeof(int)");
+	printf("Size of a long int: %lu byte(s), sizeof(long int)");
+	printf("Size of a long long int: %lu byte(s), sizeof(long long int)");
+	printf("Size of a float: %lu byte(s), sizeof(float)");
 
 	return (0);
 }
