643295695779d04a10d52d7d4802a3004182be74
Getsgoor <club-zitouni@hotmail.fr>
2025-10-20 14:51:41 +0200

    fixing for betty

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-/*Main prints a string*/
+
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
