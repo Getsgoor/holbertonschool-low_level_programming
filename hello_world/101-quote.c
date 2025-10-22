1d3648c2a6ebffb03358eb2ac76922e074fb85eb
Getsgoor <club-zitouni@hotmail.fr>
2025-10-22 20:15:21 +0200

    Fix; changed a line that was over 80 characteres

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -3,14 +3,14 @@
 
 /**
  * main- description main
- * 
+ *
  * Return: retourne 0
  */
 
 int main(void)
 {
-	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	
-	write(1, &quote, 60);
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, &quo, 60);
 	return (1);
 }
