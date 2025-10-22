6cc90515c0848642d3903744149b09db15aa9de2
Getsgoor <club-zitouni@hotmail.fr>
2025-10-22 15:21:40 +0200

    Feat: Create  function that give the absolute value of an int

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs- writes the character c to stdout
+ * @n: The character to print
+ *
+ * Return: Always 0.
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
