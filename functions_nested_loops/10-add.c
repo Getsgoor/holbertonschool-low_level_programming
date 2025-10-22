161616cad65eeb1238892606b81aa35e6381f2b7
Getsgoor <club-zitouni@hotmail.fr>
2025-10-22 18:54:49 +0200

    Feat: create a function that adds two digits and return the result

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add- writes the character c to stdout
+ * @a: The character to print
+ * @b: The other character
+ *
+ * Return: Always 0.
+ */
+
+int add(int a, int b)
+{
+	int n = a + b;
+
+	return (n);
+}
