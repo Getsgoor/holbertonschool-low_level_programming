f3ac3b78ed35f2b5e8c1869e945cb44217f72f84
Getsgoor <club-zitouni@hotmail.fr>
2025-10-22 15:01:55 +0200

    Feat: create a fonction that tell if a character is lowercase

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower- writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: Always 0.
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	return (0);
+}
