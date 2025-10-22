a61d3a5a47b91974edb75aaf795dab23738751bd
Getsgoor <club-zitouni@hotmail.fr>
2025-10-22 12:20:13 +0200

    Feat: create a c progrmm that print _pputchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "_putchar.c"
 
 /**
  * main - decription main
@@ -8,6 +9,13 @@
 
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	return (0);
 }
