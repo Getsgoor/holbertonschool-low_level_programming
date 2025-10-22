9c1894e8c1eac891b9a0c8c40f5bfbb175da4f64
Getsgoor <club-zitouni@hotmail.fr>
2025-10-22 15:15:49 +0200

    Fix: fixing betty style

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -7,19 +7,18 @@
  * Return: Always 0.
  */
 
- int print_sign(int n)
- {
+int print_sign(int n)
+{
 	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
 	}
-	if(n == 0)
+	if (n == 0)
 	{
 		_putchar('0');
 		return (0);
 	}
 	_putchar('-');
 	return (-1);
- }
- 
+}
