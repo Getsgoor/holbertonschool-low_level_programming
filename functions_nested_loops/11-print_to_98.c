5cdcdd867b7ccf09ac824d72f71dfebf12065d03
Getsgoor <club-zitouni@hotmail.fr>
2025-10-22 19:08:10 +0200

    Feat: create a function that prints all natural numbers from n to 98

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98- writes the character c to stdout
+ * @n: The character to print
+ *
+ *
+ * Return: Always 0.
+ */
+
+void print_to_98(int n)
+{
+	while (n < 98)
+	{
+		printf("%d, ", n);
+		n++;
+	}
+	while (n > 98)
+	{
+		printf("%d, ", n);
+		n--;
+	}
+	printf("98\n");
+}
