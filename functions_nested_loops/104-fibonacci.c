d91e5277ab5aa7ae322ff8215caef2a8c2e799f1
Getsgoor <club-zitouni@hotmail.fr>
2025-10-24 00:33:40 +0200

    fix: separate numbers in two instead of three

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -13,14 +13,12 @@
 void fibonacci(void)
 {
 	long int a1 = 0;
-	long int a2 = 0;
-	long int a3 = 1;
+	long int a2 = 1;
 	long int b1 = 0;
-	long int b2 = 0;
-	long int b3 = 2;
+	long int b2 = 2;
 	long int c1 = a1 + b1;
 	long int c2 = a2 + b2;
-	long int c3 = a3 + b3;
+
 	int i;
 
 	printf("1, 2");
@@ -29,19 +27,14 @@ void fibonacci(void)
 		printf(", ");
 		if (c1 != 0)
 			printf("%ld", c1);
-		if (c2 != 0)
-			printf("%ld", c2);
-		printf("%ld", c3);
+		printf("%ld", c2);
 
 		a1 = b1;
 		a2 = b2;
-		a3 = b3;
 		b1 = c1;
 		b2 = c2;
-		b3 = c3;
-		c3 = (a3 + b3) % 10000000;
-		c2 = ((a2 + b2) % 10000000) + ((a3 + b3) / 10000000);
-		c1 = ((a1 + b1) % 10000000) + ((a2 + b2) / 10000000);
+		c2 = ((a2 + b2) % 10000000000000);
+		c1 = ((a1 + b1) % 10000000000000) + ((a2 + b2) / 10000000000000);
 	}
 	printf("\n");
 }
