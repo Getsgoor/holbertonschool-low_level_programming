4c5e1d2e045645a2c4e49c296fc2bd13722297c2
Getsgoor <club-zitouni@hotmail.fr>
2025-10-23 14:11:54 +0200

    Feat create a C program that displays all combinations of two digits, in a certain way

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main- description main
+ * 
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			if (a != b && a < b)
+			{
+				putchar(a + '0');
+				putchar(b + '0');
+				if (!(a == 8 && b == 9))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
