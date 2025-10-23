78e437960edddda29111bbec788f1a9ec394233e
Getsgoor <club-zitouni@hotmail.fr>
2025-10-23 19:19:07 +0200

    Feat: create a C program that display the n times table

---
+++ functions_nested_loops/100-times_table.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98- writes the character c to stdout
+ * @n: The character to print
+ *
+ *
+ * Return: Always 0.
+ */
+
+ void print_times_table(int n)
+ {
+	int a;
+	int b;
+
+	printf("0");
+	for (a = 0; a < n; a++)
+		printf(",   0");
+	printf("\n");
+	for (a = 1; a <= n; a++)
+	{
+		printf("0");
+		for (b = 1; b <= n; b++)
+		{
+			printf(",%4.d" , a * b);
+		}
+		printf("\n");
+	}
+ }
