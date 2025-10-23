7a739f37c6c9f06e9eb9e0c06bd87bd46282e935
Getsgoor <club-zitouni@hotmail.fr>
2025-10-23 20:51:42 +0200

    Fix; don't print anything if n is negative

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -14,7 +14,7 @@ void print_times_table(int n)
 	int somme = 0;
 	int miroir = 0;
 
-	if (n > 0)
+	if (n >= 0)
 		_putchar('0');
 	for (a = 1; a <= n; a++)
 	{
@@ -24,7 +24,7 @@ void print_times_table(int n)
 		_putchar(' ');
 		_putchar('0');
 	}
-	if (n > 0)
+	if (n >= 0)
 		_putchar('\n');
 	for (a = 1; a <= n; a++)
 	{
