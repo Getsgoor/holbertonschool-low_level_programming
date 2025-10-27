b69060a726eb5e7df36e1a5754aae98dcbbe80a5
Getsgoor <club-zitouni@hotmail.fr>
2025-10-27 10:13:13 +0100

    Feat: create a function that return the product of two int

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul- multiplies to integers
+ * @a: first int
+ * @b: second int
+ *
+ * Return: result of the product
+ */
+
+int mul(int a, int b)
+{
+	return (a*b);
+}
