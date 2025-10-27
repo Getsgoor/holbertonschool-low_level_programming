89b13a36ca2bd01db3c926805a8e3fb23209cc1e
Getsgoor <club-zitouni@hotmail.fr>
2025-10-27 11:52:53 +0100

    Fix: adding just a new line if size is <= 0

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -11,6 +11,8 @@ void print_square(int size)
 {
 	int ligne, colonne;
 
+	if (size <= 0)
+		_putchar('\n');
 	for (ligne = 0; ligne < size; ligne++)
 	{
 		for (colonne = 0; colonne < size; colonne++)
