cf460e8582e5ae5396791b3e07efef0619d1b9c8
Getsgoor <club-zitouni@hotmail.fr>
2025-10-27 11:50:09 +0100

    Feat: create a funtcion that displays a square of side size

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_square- affcihe un carre
+ * @size: taille du cote
+ *
+ * Return: void
+ */
+
+void print_square(int size)
+{
+	int ligne, colonne;
+
+	for (ligne = 0; ligne < size; ligne++)
+	{
+		for (colonne = 0; colonne < size; colonne++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
