30b46ab1671514409dedfb79b585a0cc047a6fc0
Getsgoor <club-zitouni@hotmail.fr>
2025-10-27 13:36:43 +0100

    Feat: create a function that print a triangle

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_triangle: print a triangle
+ * @size:rsgf
+ *
+ * Return: 0
+ */
+
+void print_triangle(int size)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			if (j < size - i - 1)
+				_putchar(' ');
+			else
+				_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
