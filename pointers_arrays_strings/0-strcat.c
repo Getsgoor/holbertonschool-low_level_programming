6a6ab15f9bac87ed5cc8168be713158482cfd984
Getsgoor <club-zitouni@hotmail.fr>
2025-10-28 15:37:03 +0100

    Feat: create a function that concatenate two strings

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * _strcat- concatenates two string
+ * @dest: the string to return
+ * @src: the string to concatenatel
+ *
+ * Return:
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int tailleDest = _strlen(dest);
+	int tailleSrc = _strlen(src);
+	int i;
+
+	for (i = 0; i < tailleSrc; i++)
+	{
+		dest[tailleDest + i] = src[i];
+	}
+
+	return dest;
+}
