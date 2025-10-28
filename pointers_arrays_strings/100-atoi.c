b96e509f30d213b95200499bf45ef4964be577c7
Getsgoor <club-zitouni@hotmail.fr>
2025-10-28 09:54:51 +0100

    Feat: create a fucntion that convert a string to an int

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdbool.h>
+
+/**
+ * _atoi- convert a string to int
+ * @s: the string to convert
+ *
+ * Return: an int
+ */
+
+int _atoi(char *s)
+{
+	int i = 0;
+	bool positif = true;
+	int retour = 0;
+
+	while (!(s[i] >= '0' && s[i] <= '9') && s[i] != '\0')
+	{
+		if (s[i] == '-')
+			positif = !positif;
+		i++;
+	}
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		retour += s[i] - '0';
+		retour *= 10;
+		i++;
+	}
+	if (!positif)
+		retour *= (-1);
+	return (retour /= 10);
+}
