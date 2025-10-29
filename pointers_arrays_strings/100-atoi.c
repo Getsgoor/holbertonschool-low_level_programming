dc028cff980ba75f80a4ebb6d5556de323e1b42b
Getsgoor <club-zitouni@hotmail.fr>
2025-10-29 10:45:46 +0100

    Fix: using macro INT_MAX

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdbool.h>
+#include <limits.h>
 
 /**
  * _atoi- convert a string to int
@@ -13,7 +14,7 @@ int _atoi(char *s)
 	int i = 0;
 	bool positif = true;
 	int retour = 0;
-
+	
 	while (!(s[i] >= '0' && s[i] <= '9') && s[i] != '\0')
 	{
 		if (s[i] == '-')
@@ -25,7 +26,10 @@ int _atoi(char *s)
 		retour += s[i] - '0';
 		retour *= 10;
 		i++;
+
 	}
+	if (retour < 0)
+		retour = INT_MAX;
 	if (!positif)
 		retour *= (-1);
 	return (retour /= 10);
