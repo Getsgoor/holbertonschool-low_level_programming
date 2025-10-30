daee5d9bc9b72a4c15546280df1e1fd2cbf71f1e
Getsgoor <club-zitouni@hotmail.fr>
2025-10-30 22:10:59 +0100

    Fic betty

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -4,20 +4,19 @@
  * reverse_array(int *a, int n)- inverse une liste
  * @a: liste
  * @n: taille de la liste
- * 
+ *
  * Return: void
  */
 
- void reverse_array(int *a, int n)
- {
-    int i;
-    int temp;
+void reverse_array(int *a, int n)
+{
+	int i;
+	int temp;
 
 	for (i = 0; i < n / 2; i++)
 	{
-        temp = a[i];
-        a[i] = a[n - 1 -i];
-        a[n - 1 -i] = temp;
+		temp = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = temp;
 	}
- }
- 
+}
