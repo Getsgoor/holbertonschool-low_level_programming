2947b7708ecde792d8a2d2c3cd9d78fc7334ca4a
Getsgoor <club-zitouni@hotmail.fr>
2025-10-30 22:08:48 +0100

    create a function that capitalize all words of a stringé

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdbool.h>
+
+/**
+ * cap_string- capitalize all words
+ * @str: the string
+ *
+ * Return: the string
+ */
+
+char *cap_string(char *str)
+{
+	bool nouveauMot = false;
+
+	for (; *str != '\0'; str++)
+	{
+		if (*str == 44  ||  *str == 59 || *str == ' '  || *str == '\n'  ||
+		*str == '\t'  ||  *str == 63  ||  *str == 46  ||  *str == 33  ||
+		*str == 34  ||  *str == 40  ||  *str == 41  ||  *str == 123  ||
+		*str == 125)
+		{
+			nouveauMot = true;
+		}
+		if ((nouveauMot  &&  *str >= 65  &&  *str <= 90)  ||
+		(*str <= '9'  &&  *str >= '0'))
+		{
+			nouveauMot = false;
+		}
+		if (nouveauMot  &&  *str >= 97  &&  *str <= 122)
+		{
+			*str -= 32;
+			nouveauMot = false;
+		}
+	}
+	return (str);
+}
