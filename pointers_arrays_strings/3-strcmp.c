48735ff18d47b9aa73d2dcb6ec05a21611481dd3
Getsgoor <club-zitouni@hotmail.fr>
2025-10-30 20:40:26 +0100

    Fix: fix betty

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -4,17 +4,16 @@
  * _strcmp- comparees two strings
  * @s1: 1er string
  * @s2: 2eme string
- * 
+ *
  * Return: compare
  */
 
- int _strcmp(char *s1, char *s2)
- {
-	for (;*s1 != '\0'  &&  *s2 != '\0'; s1++, s2++)
+int _strcmp(char *s1, char *s2)
+{
+	for (; *s1 != '\0' && *s2 != '\0'; s1++, s2++)
 	{
 		if (*s1 != *s2)
 			return (*s1 - *s2);
 	}
 	return (0);
-	
- }
+}
