cf1e01483eeb2ebe6f6493576282517d92f9f86a
Getsgoor <club-zitouni@hotmail.fr>
2025-10-30 22:51:20 +0100

    Fix : create a neew pointer

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -10,6 +10,7 @@
 char *cap_string(char *str)
 {
 	char separators[] = " \t\n,;.!?\"(){}";
+	char *ret = str;
 
 	if (*str >= 97  &&  *str <= 122)
 	{
@@ -19,7 +20,7 @@ char *cap_string(char *str)
 	while (*str++ != '\0')
 	{
 		int i = 0;
-
+		
 		while (separators[i] != '\0')
 		{
 			if (*str == separators[i]  &&  *(str + 1) >= 'a'  &&  *(str + 1) <= 'z')
@@ -30,5 +31,5 @@ char *cap_string(char *str)
 			i++;
 		}
 	}
-	return (str);
+	return (ret);
 }
