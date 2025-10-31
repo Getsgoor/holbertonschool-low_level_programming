bd964e3f040aa22008c00d2631ff02af8b6d0b33
Getsgoor <club-zitouni@hotmail.fr>
2025-10-31 10:00:09 +0100

    correct the name of the file

---
+++ pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_number- prints an integer
+ * @n: fz
+ *
+ * return: void
+ */
+
+void print_number(int n)
+{
+	int taille = 1;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+	else if (n == 0)
+		_putchar('0');
+	while (n / taille != 0)
+	{
+		taille *= 10;
+	}
+	taille /= 10;
+
+	while (taille > 0)
+	{
+		_putchar(((n / taille) % 10) + '0');
+		taille /= 10;
+	}
+}
