f6d3f44390c56d134f4eb30ed9951c8176c34748
Getsgoor <club-zitouni@hotmail.fr>
2025-11-03 20:23:33 +0100

    Feat: create a function that finds a caractere in a string

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strchr-df
+ * @s:s
+ * @c:c
+ *
+ * Return: ptr
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return ('\0');
+}
