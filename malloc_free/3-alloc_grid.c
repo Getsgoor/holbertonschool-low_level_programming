3830150117ed189e6fec7ac510d6e42931c53ad8
Getsgoor <club-zitouni@hotmail.fr>
2025-11-10 14:27:40 +0100

    Fix: frees all the mallocs

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -19,11 +19,21 @@ int **alloc_grid(int width, int height)
 
 	grid = malloc(sizeof(int *) * height);
 	if (grid == NULL)
+	{
+		free(grid);
 		return (NULL);
+	}
 
 	for (i = 0; i < height; i++)
 	{
 		grid[i] = malloc(sizeof(int) * width);
+		if (grid[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+				free(grid[j]);
+			free(grid);
+			return (NULL);
+		}
 		for (j = 0; j < width; j++)
 		{
 			grid[i][j] = 0;
