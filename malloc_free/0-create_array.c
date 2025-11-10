3e20505f9ba49e4f02bc2dea0a0eecad8f2d02ac
Getsgoor <club-zitouni@hotmail.fr>
2025-11-10 12:31:15 +0100

    Feat: create a function that creates a string with malloc

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array- create a string
+ * @size: size
+ * @c: character that fills the array
+ *
+ * Return: the string
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *str;
+	unsigned int i;
+
+	str = malloc(sizeof(char) * size);
+	if (size <= 0)
+	{
+		return (NULL);
+	}
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		str[i] = c;
+	}
+
+	return (str);
+}
